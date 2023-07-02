05c83e25f7b944ea553e7ac82aa9f5d3793cdfdb
MetaZoor <defi0078@protonmail.com>
2023-07-02 18:00:52 +0300

    Create Variables and Operators.py

---
+++ Variables and Operators.py
@@ -0,0 +1,23 @@
+## Learning how to use variables of different kinds
+x = 1
+y = 1.2
+
+a = x + y 
+b = x - y 
+c = x * y 
+d = x / y 
+
+## % // - This is the floor division operator. It divides the left operand by the right operand and rounds the result down to the nearest whole number.
+e = x // y 
+
+## % - This is the modulus operator.
+f = x % y
+
+## ** - This is the exponentiation operator.
+g = x ** y 
+
+x_equals_y = (x == y)
+
+z = "Commandar Erwin Smith"
+
+print (a, b, c, d, e, f, g, z, sep="\n")
